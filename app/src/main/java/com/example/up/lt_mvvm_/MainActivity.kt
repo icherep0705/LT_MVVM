2ac5108f4eb2c8fb0b3fb40d0ae0ee70fcdbfd9d
Ivan Cherepanin <1cherep11@gmail.com>
2021-03-10 13:37:04 -0500

    Initial commit

---
+++ app/src/main/java/com/example/up/lt_mvvm_/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.up.lt_mvvm_
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
