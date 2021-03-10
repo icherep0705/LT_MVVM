5ecb2bbcbeee96745720a5dbda4e7a50f1afc76e
Ivan Cherepanin <1cherep11@gmail.com>
2021-03-10 14:10:25 -0500

    Add LTmvvmApplication and Service classes

---
+++ app/src/main/java/com/example/up/lt_mvvm_/LTmvvmApplication.kt
@@ -0,0 +1,8 @@
+package com.example.up.lt_mvvm_
+
+import android.app.Application
+import android.content.Context
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+open class LTmvvmApplication : Application()
---
+++ app/src/main/java/com/example/up/lt_mvvm_/model/Service.kt
@@ -0,0 +1,60 @@
+package com.example.up.lt_mvvm_.model
+
+import com.example.up.lt_mvvm_.BuildConfig
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+import javax.inject.Inject
+import javax.inject.Singleton
+
+
+private const val timeoutRead = 30
+private const val contentType = "Content-Type"
+private const val contentTypeValue = "application/json"
+private const val timeoutConnect = 30
+
+@Singleton
+class Service @Inject constructor() {
+    private val okHttpBuilder: OkHttpClient.Builder = OkHttpClient.Builder()
+    private val retrofit: Retrofit
+
+    private var headerInterceptor = Interceptor { chain ->
+        val original = chain.request()
+
+        val request = original.newBuilder()
+                .header(contentType, contentTypeValue)
+                .method(original.method, original.body)
+                .build()
+
+        chain.proceed(request)
+    }
+
+    private val logger: HttpLoggingInterceptor
+        get() {
+            val loggingInterceptor = HttpLoggingInterceptor()
+            if (BuildConfig.DEBUG) {
+                loggingInterceptor.apply { level = HttpLoggingInterceptor.Level.BODY }
+            }
+            return loggingInterceptor
+        }
+
+    init {
+        okHttpBuilder.addInterceptor(headerInterceptor)
+        okHttpBuilder.addInterceptor(logger)
+        okHttpBuilder.connectTimeout(timeoutConnect.toLong(), TimeUnit.SECONDS)
+        okHttpBuilder.readTimeout(timeoutRead.toLong(), TimeUnit.SECONDS)
+        val client = okHttpBuilder.build()
+        retrofit = Retrofit.Builder()
+                .baseUrl("BASE_URL").client(client)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+    }
+
+    fun <S> createService(serviceClass: Class<S>): S {
+        return retrofit.create(serviceClass)
+    }
+
+}
