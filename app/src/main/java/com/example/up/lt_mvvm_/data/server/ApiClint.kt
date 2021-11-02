dec937692892f88dfc885f4fd44ce5913392d068
Ivan Cherepanin <Ivan.Cherepanin@kiswe.com>
2021-11-02 10:40:21 -0400

    wip

--- app/src/main/java/com/example/up/lt_mvvm_/data/server/ApiClint.kt
+++ app/src/main/java/com/example/up/lt_mvvm_/data/server/ApiClint.kt
@@ -8,8 +8,10 @@ import retrofit2.http.Query
 interface ApiClient {
 
     //GET https://api.exchangeratesapi.io/latest?base=USD
+    //GET https://api.exchangeratesapi.io/latest?base=USD
 
     @GET("latest")
+//    @GET("latest")
     fun getRates(
         @Query("base") currency: String?): Deferred<Currency>
 }
